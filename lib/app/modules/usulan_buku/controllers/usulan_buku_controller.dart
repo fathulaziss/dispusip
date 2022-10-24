41613f25229f9e33db671a28fe945961d43881d4
Muhamad Fathul Azis <azisfathl@gmail.com>
2022-10-24 20:41:09 +0700

    integrasi api menu usulan buku done

--- lib/app/modules/usulan_buku/controllers/usulan_buku_controller.dart
+++ lib/app/modules/usulan_buku/controllers/usulan_buku_controller.dart
@@ -1,3 +1,153 @@
+import 'package:dispusip/services/api_service.dart';
+import 'package:dispusip/styles/styles.dart';
+import 'package:dispusip/utils/app_utils.dart';
+import 'package:dispusip/widgets/others/show_dialog.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class UsulanBukuController extends GetxController {}
+class UsulanBukuController extends GetxController {
+  final cJudulBuku = TextEditingController();
+  RxString judulBuku = ''.obs;
+  RxBool isValidJudulBuku = false.obs;
+
+  final cPengarang = TextEditingController();
+  RxString pengarang = ''.obs;
+  RxBool isValidPengarang = false.obs;
+
+  final cPenerbit = TextEditingController();
+  RxString penerbit = ''.obs;
+  RxBool isValidPenerbit = false.obs;
+
+  final cKotaTerbit = TextEditingController();
+  RxString kotaTerbit = ''.obs;
+  RxBool isValidKotaTerbit = false.obs;
+
+  final cTahunTerbit = TextEditingController();
+  RxString tahunTerbit = ''.obs;
+  RxBool isValidTahunTerbit = false.obs;
+
+  final cKomentar = TextEditingController();
+  RxString komentar = ''.obs;
+  RxBool isValidKomentar = false.obs;
+
+  RxBool isLoading = false.obs;
+  RxBool isValidForm = false.obs;
+
+  void setJudulBuku(String value) {
+    judulBuku(value);
+    if (judulBuku.value.isNotEmpty) {
+      isValidJudulBuku(true);
+    } else {
+      isValidJudulBuku(false);
+    }
+    validate();
+  }
+
+  void setPengarang(String value) {
+    pengarang(value);
+    if (pengarang.value.isNotEmpty) {
+      isValidPengarang(true);
+    } else {
+      isValidPengarang(false);
+    }
+    validate();
+  }
+
+  void setPenerbit(String value) {
+    penerbit(value);
+    if (penerbit.value.isNotEmpty) {
+      isValidPenerbit(true);
+    } else {
+      isValidPenerbit(false);
+    }
+    validate();
+  }
+
+  void setKotaTerbit(String value) {
+    kotaTerbit(value);
+    if (kotaTerbit.value.isNotEmpty) {
+      isValidKotaTerbit(true);
+    } else {
+      isValidKotaTerbit(false);
+    }
+    validate();
+  }
+
+  void setTahunTerbit(int value) {
+    tahunTerbit('$value');
+    if (tahunTerbit.value.isNotEmpty) {
+      isValidTahunTerbit(true);
+    } else {
+      isValidTahunTerbit(false);
+    }
+    validate();
+  }
+
+  void setKomentar(String value) {
+    komentar(value);
+    if (komentar.value.isNotEmpty) {
+      isValidKomentar(true);
+    } else {
+      isValidKomentar(false);
+    }
+    validate();
+  }
+
+  void validate() {
+    if (isValidJudulBuku.value &&
+        isValidPengarang.value &&
+        isValidPenerbit.value &&
+        isValidKotaTerbit.value &&
+        isValidTahunTerbit.value &&
+        isValidKomentar.value) {
+      isValidForm(true);
+    } else {
+      isValidForm(false);
+    }
+  }
+
+  Future<void> submit() async {
+    try {
+      isLoading(true);
+
+      final parameters = {'': ''};
+
+      final r = await ApiService().request(
+        url: 'book/usulan',
+        method: Method.POST,
+        parameters: parameters,
+      );
+
+      isLoading(false);
+
+      showPopUpInfo(title: 'Success', description: r['message']);
+
+      resetForm();
+    } catch (e) {
+      isLoading(false);
+      logSys(e.toString());
+    }
+  }
+
+  void resetForm() {
+    cJudulBuku.clear();
+    cPengarang.clear();
+    cPenerbit.clear();
+    cKotaTerbit.clear();
+    cTahunTerbit.clear();
+    cKomentar.clear();
+    isValidJudulBuku(false);
+    isValidPengarang(false);
+    isValidPenerbit(false);
+    isValidKotaTerbit(false);
+    isValidTahunTerbit(false);
+    isValidKomentar(false);
+    isValidForm(false);
+    judulBuku('');
+    pengarang('');
+    penerbit('');
+    kotaTerbit('');
+    tahunTerbit('');
+    komentar('');
+  }
+}
--- lib/app/modules/usulan_buku/views/usulan_buku_view.dart
+++ lib/app/modules/usulan_buku/views/usulan_buku_view.dart
@@ -16,107 +16,129 @@ class UsulanBukuView extends GetView<UsulanBukuController> {
 
   @override
   Widget build(BuildContext context) {
-    return PageDefaultTwo(
-      titlePage: 'Usulan Buku',
-      isShowButtonBottom: true,
-      buttonBottom: ButtonPrimary(
-        onTap: () {},
-        label: 'SIMPAN',
-      ),
-      child: Column(
-        children: [
-          Text(
-            'Silahkan isikan form dibawah ini untuk mengajukan usulan buku.',
-            style: TextStyles.desc,
-          ),
-          verticalSpace(Insets.lg),
-          InputPrimary(
-            label: 'Judul Buku',
-            hint: 'Masukkan Judul Buku',
-            contentPadding: EdgeInsets.all(Insets.sm),
-            inputStyle: InputStyle.line,
-            prefixIcon: Padding(
-              padding: EdgeInsets.symmetric(horizontal: Insets.sm),
-              child: SizedBox.square(
-                dimension: 18.w,
-                child: Image.asset(AppAsset.icon('ic_judul_buku.png')),
+    return Obx(
+      () => PageDefaultTwo(
+        titlePage: 'Usulan Buku',
+        isShowButtonBottom: true,
+        buttonBottom: ButtonPrimary(
+          onTap: controller.submit,
+          label: 'SIMPAN',
+          isLoading: controller.isLoading.value,
+          enabled: controller.isValidForm.value,
+        ),
+        child: Column(
+          children: [
+            Text(
+              'Silahkan isikan form dibawah ini untuk mengajukan usulan buku.',
+              style: TextStyles.desc,
+            ),
+            verticalSpace(Insets.lg),
+            InputPrimary(
+              label: 'Judul Buku',
+              hint: 'Masukkan Judul Buku',
+              controller: controller.cJudulBuku,
+              onChanged: controller.setJudulBuku,
+              validation: (value) => true,
+              contentPadding: EdgeInsets.all(Insets.sm),
+              inputStyle: InputStyle.line,
+              prefixIcon: Padding(
+                padding: EdgeInsets.symmetric(horizontal: Insets.sm),
+                child: SizedBox.square(
+                  dimension: 18.w,
+                  child: Image.asset(AppAsset.icon('ic_judul_buku.png')),
+                ),
               ),
             ),
-          ),
-          verticalSpace(Insets.lg),
-          InputPrimary(
-            label: 'Pengarang',
-            hint: 'Masukkan Nama Pengarang',
-            contentPadding: EdgeInsets.all(Insets.sm),
-            inputStyle: InputStyle.line,
-            prefixIcon: Padding(
-              padding: EdgeInsets.symmetric(horizontal: Insets.sm),
-              child: SizedBox.square(
-                dimension: 18.w,
-                child: Image.asset(AppAsset.icon('ic_author.png')),
+            verticalSpace(Insets.lg),
+            InputPrimary(
+              label: 'Pengarang',
+              hint: 'Masukkan Nama Pengarang',
+              controller: controller.cPengarang,
+              onChanged: controller.setPengarang,
+              validation: (value) => true,
+              contentPadding: EdgeInsets.all(Insets.sm),
+              inputStyle: InputStyle.line,
+              prefixIcon: Padding(
+                padding: EdgeInsets.symmetric(horizontal: Insets.sm),
+                child: SizedBox.square(
+                  dimension: 18.w,
+                  child: Image.asset(AppAsset.icon('ic_author.png')),
+                ),
               ),
             ),
-          ),
-          verticalSpace(Insets.lg),
-          InputPrimary(
-            label: 'Penerbit',
-            hint: 'Masukkan Penerbit',
-            contentPadding: EdgeInsets.all(Insets.sm),
-            inputStyle: InputStyle.line,
-            prefixIcon: Padding(
-              padding: EdgeInsets.symmetric(horizontal: Insets.sm),
-              child: SizedBox.square(
-                dimension: 18.w,
-                child: Image.asset(AppAsset.icon('ic_publisher.png')),
+            verticalSpace(Insets.lg),
+            InputPrimary(
+              label: 'Penerbit',
+              hint: 'Masukkan Penerbit',
+              controller: controller.cPenerbit,
+              onChanged: controller.setPenerbit,
+              validation: (value) => true,
+              contentPadding: EdgeInsets.all(Insets.sm),
+              inputStyle: InputStyle.line,
+              prefixIcon: Padding(
+                padding: EdgeInsets.symmetric(horizontal: Insets.sm),
+                child: SizedBox.square(
+                  dimension: 18.w,
+                  child: Image.asset(AppAsset.icon('ic_publisher.png')),
+                ),
               ),
             ),
-          ),
-          verticalSpace(Insets.lg),
-          InputPrimary(
-            label: 'Kota Terbit',
-            hint: 'Masukkan Kota Terbit',
-            contentPadding: EdgeInsets.all(Insets.sm),
-            inputStyle: InputStyle.line,
-            prefixIcon: Padding(
-              padding: EdgeInsets.symmetric(horizontal: Insets.sm),
-              child: SizedBox.square(
-                dimension: 18.w,
-                child: Image.asset(AppAsset.icon('ic_location.png')),
+            verticalSpace(Insets.lg),
+            InputPrimary(
+              label: 'Kota Terbit',
+              hint: 'Masukkan Kota Terbit',
+              controller: controller.cKotaTerbit,
+              onChanged: controller.setKotaTerbit,
+              validation: (value) => true,
+              contentPadding: EdgeInsets.all(Insets.sm),
+              inputStyle: InputStyle.line,
+              prefixIcon: Padding(
+                padding: EdgeInsets.symmetric(horizontal: Insets.sm),
+                child: SizedBox.square(
+                  dimension: 18.w,
+                  child: Image.asset(AppAsset.icon('ic_location.png')),
+                ),
               ),
             ),
-          ),
-          verticalSpace(Insets.lg),
-          InputNumber(
-            label: 'Tahun Terbit',
-            hint: 'Masukkan Tahun Terbit',
-            contentPadding: EdgeInsets.all(Insets.sm),
-            inputStyle: InputStyle.line,
-            prefixIcon: Padding(
-              padding: EdgeInsets.symmetric(horizontal: Insets.sm),
-              child: SizedBox.square(
-                dimension: 18.w,
-                child: Image.asset(AppAsset.icon('ic_calendar.png')),
+            verticalSpace(Insets.lg),
+            InputNumber(
+              label: 'Tahun Terbit',
+              hint: 'Masukkan Tahun Terbit',
+              controller: controller.cTahunTerbit,
+              value: controller.setTahunTerbit,
+              validation: (value) => true,
+              contentPadding: EdgeInsets.all(Insets.sm),
+              inputStyle: InputStyle.line,
+              prefixIcon: Padding(
+                padding: EdgeInsets.symmetric(horizontal: Insets.sm),
+                child: SizedBox.square(
+                  dimension: 18.w,
+                  child: Image.asset(AppAsset.icon('ic_calendar.png')),
+                ),
               ),
             ),
-          ),
-          verticalSpace(Insets.lg),
-          InputPrimary(
-            label: 'Komentar',
-            hint: 'Masukkan Komentar',
-            contentPadding: EdgeInsets.all(Insets.sm),
-            inputStyle: InputStyle.line,
-            maxLines: null,
-            minLines: null,
-            prefixIcon: Padding(
-              padding: EdgeInsets.symmetric(horizontal: Insets.sm),
-              child: SizedBox.square(
-                dimension: 18.w,
-                child: Image.asset(AppAsset.icon('ic_comment.png')),
+            verticalSpace(Insets.lg),
+            InputPrimary(
+              label: 'Komentar',
+              hint: 'Masukkan Komentar',
+              controller: controller.cKomentar,
+              onChanged: controller.setKomentar,
+              validation: (value) => true,
+              contentPadding: EdgeInsets.all(Insets.sm),
+              inputStyle: InputStyle.line,
+              maxLines: null,
+              minLines: null,
+              prefixIcon: Padding(
+                padding: EdgeInsets.symmetric(horizontal: Insets.sm),
+                child: SizedBox.square(
+                  dimension: 18.w,
+                  child: Image.asset(AppAsset.icon('ic_comment.png')),
+                ),
               ),
             ),
-          ),
-          verticalSpace(Insets.lg),
-        ],
+            verticalSpace(Insets.lg),
+          ],
+        ),
       ),
     );
   }
