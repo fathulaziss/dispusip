a43361c93baecbd82604e887b77fae443fffb928
Muhamad Fathul Azis <azisfathl@gmail.com>
2022-11-18 03:03:17 +0700

    update under development view

--- lib/app/modules/under_development_view.dart
+++ lib/app/modules/under_development_view.dart
@@ -1,6 +1,9 @@
-import 'package:dispusip/styles/colors.dart';
+import 'package:dispusip/styles/styles.dart';
 import 'package:dispusip/utils/app_asset.dart';
+import 'package:dispusip/widgets/buttons/button_primary.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 
 class UnderDevelopmentView extends StatelessWidget {
   const UnderDevelopmentView({Key? key}) : super(key: key);
@@ -8,9 +11,31 @@ class UnderDevelopmentView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColor.primaryColor,
-      body: Center(
-        child: Image.asset(AppAsset.image('img_under_development.png')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            AppAsset.image('img_under_development.png'),
+            width: 250.w,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.w),
+            child: Text(
+              'Fitur ini Sedang Dalam Proses Pengembangan',
+              style: TextStyles.text.copyWith(fontSize: 16.w),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          verticalSpace(Insets.med),
+          ButtonPrimary(
+            label: 'Kembali',
+            width: 150.w,
+            radius: 12.w,
+            textStyle:
+                TextStyles.title.copyWith(fontSize: 16.w, color: Colors.white),
+            onTap: Get.back,
+          ),
+        ],
       ),
     );
   }
